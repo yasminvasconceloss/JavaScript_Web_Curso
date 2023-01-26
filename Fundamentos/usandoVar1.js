daa9c4af0b5c2063c5ddf986ed2130320d6901d4
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2023-01-26 02:28:09 -0300

    Diferenças Var e Let

---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,16 @@
+{{{{{var sera = 'Será'
+    console.log(sera)
+}}}}}
+
+console.log(sera)
+
+
+
+function teste() {
+    var local = 123
+    console.log(local) 
+
+}
+
+teste()
+//console.log(local)  //não estará visível fora da função
---
+++ Fundamentos/usandoVar2.js
@@ -0,0 +1,7 @@
+var numero = 1
+{
+    var numero = 2                       //subescreveu valor de 1
+    console.log('dentro =', numero) 
+}
+
+console.log('fora =', numero) 
